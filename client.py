d30560dd40c441ca6fc39b9af1392fcc3738d719
FernandoJelvez <f.jelvez02@ufromail.cl>
2026-08-26 22:14:12 -0400

    main loop and added connection reset handling to server

--- client.py
+++ client.py
@@ -3,8 +3,11 @@ serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-sentence = input('Escribe una frase en minúsculas:')
-clientSocket.send(sentence.encode())
-receivedSentence = clientSocket.recv(1024)
-print('From Server: ', receivedSentence.decode())
+while True:
+    sentence = input('Escribe una frase en minúsculas:')
+    if sentence == '//exit':
+        break
+    clientSocket.send(sentence.encode())
+    receivedSentence = clientSocket.recv(1024)
+    print('From Server: ', receivedSentence.decode())
 clientSocket.close()
--- server.py
+++ server.py
@@ -2,10 +2,16 @@ from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
 print('El servidor está listo para recibir')
+serverSocket.listen(1)
 while True:
     connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode()
-    connectionSocket.send(sentence.encode())
+    while True:
+        try:
+            sentence = connectionSocket.recv(1024).decode()
+            if sentence=="":
+                break
+            connectionSocket.send(sentence.encode())
+        except ConnectionResetError:
+            break
     connectionSocket.close()
